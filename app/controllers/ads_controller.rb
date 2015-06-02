dd9c1db3e5203bb4cb8a536574490122c14bec66
Colin Treseler <treseler@gmail.com>
2015-06-02 14:35:56 +0200

    heroku ready

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -3,7 +3,7 @@ class AdsController < ApplicationController
   def index
     puts "hiiiii"
     puts request.user_agent
-
+    
     if request.user_agent.include? "iPhone"
       puts "OMG ITS AN IPHONE"
       redirect_to url("instagram://user?username=colintres")
