91c6d853180d5a28cf0c604d62f6674378d425e9
Colin Treseler <treseler@gmail.com>
2015-06-02 14:46:42 +0200

    redirect

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -3,10 +3,10 @@ class AdsController < ApplicationController
   def index
     puts "hiiiii"
     puts request.user_agent
-    
+
     if request.user_agent.include? "iPhone"
       puts "OMG ITS AN IPHONE"
-      redirect_to url("instagram://user?username=colintres")
+      redirect_to "instagram://user?username=colintres"
     end
   end
 
