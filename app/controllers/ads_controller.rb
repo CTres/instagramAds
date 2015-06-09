b62cc33918db6f71b191c45bdf570ea84010652c
Colin Treseler <treseler@gmail.com>
2015-06-09 08:42:17 +0200

    go tags

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,13 +1,25 @@
 class AdsController < ApplicationController
+  before_filter :create_url, only: :index
 
   def index
-    puts "hiiiii"
-    puts request.user_agent
 
+    puts request.user_agent
     if request.user_agent.include? "iPhone"
       render "iphone.js"
+    else
+      redirect_to @url
     end
   end
 
+  def lookup
+    uri = URI(request.original_url)
+    @name= params[:path]
+  end
 
+
+
+
+  def create_url()
+    @url = 'https://www.instagram.com/' + params[:username]
+  end
 end
--- app/views/ads/iphone.js
+++ app/views/ads/iphone.js
@@ -1,5 +1,5 @@
 
 <script type="text/javascript">
-  setTimeout(function () { window.location = "https://www.instagram.com/colintres"; }, 25);
-  window.location = "instagram://user?username=colintres";
+  setTimeout(function () { window.location = "https://www.instagram.com/" + @username; }, 25);
+  window.location = "instagram://user?username=" + @username;
 </script>
